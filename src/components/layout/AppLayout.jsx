adf38605878a2f71021193f52b4fa6dca6fce423
tonjeedwards-max <tonje.edwards@gmail.com>
2026-08-13 12:40:27 -0500

    Add AppLayout component with layout structure

---
+++ src/components/layout/AppLayout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import FloatingActions from "@/components/shared/FloatingActions";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import BottomNav from "./BottomNav";
+import StarField from "../shared/StarField";
+import SplashScreen from "@/components/SplashScreen";
+import ChatWidget from "@/components/blog/ChatWidget";
+import CopyProtect from "@/components/shared/CopyProtect";
+
+export default function AppLayout() {
+  return (
+    <div className="min-h-screen flex flex-col relative pb-16 lg:pb-0">
+      <StarField />
+      <SplashScreen />
+      <Navbar />
+      <main className="flex-1 pt-16 overflow-x-hidden">
+        <Outlet />
+      </main>
+      <Footer />
+      <CopyProtect />
+      <BottomNav />
+      <FloatingActions />
+      <ChatWidget />
+    </div>
+  );
+}
