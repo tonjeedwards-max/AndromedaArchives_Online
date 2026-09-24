fa9ad4a05e072796f7c6d676c834d7d871bb5cda
tonjeedwards-max <tonje.edwards@gmail.com>
2026-09-24 12:48:42 -0500

    Keep podcast player alive across navigation

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -7,20 +7,23 @@ import Footer from "./Footer";
 import BottomNav from "./BottomNav";
 import StarField from "../shared/StarField";
 import CopyProtect from "@/components/shared/CopyProtect";
+import { PodcastPlayerProvider } from "@/components/podcasts/PodcastPlayerContext";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen flex flex-col relative pb-16 lg:pb-0">
-      <StarField />
-      <Navbar />
-      <main className="flex-1 pt-16 overflow-x-hidden">
-        <Outlet />
-      </main>
-      <Footer />
-      <CopyProtect />
-      <BottomNav />
-      <ChatWidget />
-      <FloatingActions />
-    </div>
+    <PodcastPlayerProvider>
+      <div className="min-h-screen flex flex-col relative pb-16 lg:pb-0">
+        <StarField />
+        <Navbar />
+        <main className="flex-1 pt-16 overflow-x-hidden">
+          <Outlet />
+        </main>
+        <Footer />
+        <CopyProtect />
+        <BottomNav />
+        <ChatWidget />
+        <FloatingActions />
+      </div>
+    </PodcastPlayerProvider>
   );
 }
